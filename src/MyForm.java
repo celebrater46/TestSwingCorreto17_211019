12d632c6f8d6e2a360ab1445793a495e823cb291
hideru <jota.16.stigma.world@gmail.com>
2021-10-19 21:29:55 +0900

    Hello World after pushing the button succeeded

--- src/MyForm.java
+++ src/MyForm.java
@@ -5,12 +5,14 @@ import java.awt.event.MouseEvent;
 public class MyForm extends JFrame {
     private JButton clickMeButton;
     private JPanel panel1;
+    private JLabel label1;
 
     public MyForm() {
         clickMeButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
+                label1.setText("Hello World.");
             }
         });
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
